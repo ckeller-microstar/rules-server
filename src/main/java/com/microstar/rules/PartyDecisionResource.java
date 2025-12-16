63b520343806ee9bc4d261ce5cc5b5122d70915f
Chris Keller <chris.keller@microstarkegs.com>
2025-12-16 08:54:48 -0700

    Add Swagger UI

--- src/main/java/com/microstar/rules/PartyDecisionResource.java
+++ src/main/java/com/microstar/rules/PartyDecisionResource.java
@@ -1,13 +1,15 @@
 package com.microstar.rules;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 import org.kie.dmn.api.core.DMNContext;
 import org.kie.dmn.api.core.DMNModel;
 import org.kie.dmn.api.core.DMNResult;
@@ -18,48 +20,54 @@ import org.kie.dmn.api.core.DMNRuntime;
 @Consumes(MediaType.APPLICATION_JSON)
 public class PartyDecisionResource {
 
-    private final DMNRuntime dmnRuntime;
-    private final DMNModel dmnModel;
+  private final DMNRuntime dmnRuntime;
+  private final DMNModel dmnModel;
 
-    public PartyDecisionResource() {
-        KieServices kieServices = KieServices.Factory.get();
-        KieContainer kieContainer = kieServices.getKieClasspathContainer();
-        this.dmnRuntime = kieContainer.newKieSession().getKieRuntime(DMNRuntime.class);
-        this.dmnModel = dmnRuntime.getModel("https://kie.org/dmn/_C550657A-1516-4B94-92A5-8613A052F7EE", "DMN_318F5DE4-BE6D-4DB7-B371-7ED04F6CE0DD");
+  public PartyDecisionResource() {
+    KieServices kieServices = KieServices.Factory.get();
+    KieContainer kieContainer = kieServices.getKieClasspathContainer();
+    KieSession kieSession = kieContainer.newKieSession();
+    this.dmnRuntime = kieSession.getKieRuntime(DMNRuntime.class);
+    this.dmnModel =
+        dmnRuntime.getModel(
+            "https://kie.org/dmn/_C550657A-1516-4B94-92A5-8613A052F7EE",
+            "DMN_318F5DE4-BE6D-4DB7-B371-7ED04F6CE0DD");
+  }
+
+  @POST
+  @Path("/decide")
+  @Operation(summary = "Make party decision based on day of week")
+  @APIResponse(responseCode = "200", description = "Party decision made")
+  public PartyResponse decideParty(PartyRequest request) {
+    DMNContext dmnContext = dmnRuntime.newContext();
+    dmnContext.set("Day of the week", request.dayOfWeek);
+
+    DMNResult dmnResult = dmnRuntime.evaluateAll(dmnModel, dmnContext);
+
+    String partyDecision = (String) dmnResult.getDecisionResultByName("Party").getResult();
+
+    return new PartyResponse(request.dayOfWeek, partyDecision);
+  }
+
+  public static class PartyRequest {
+    public String dayOfWeek;
+
+    public PartyRequest() {}
+
+    public PartyRequest(String dayOfWeek) {
+      this.dayOfWeek = dayOfWeek;
     }
+  }
 
-    @POST
-    @Path("/decide")
-    public PartyResponse decideParty(PartyRequest request) {
-        DMNContext dmnContext = dmnRuntime.newContext();
-        dmnContext.set("Day of the week", request.dayOfWeek);
+  public static class PartyResponse {
+    public String dayOfWeek;
+    public String partyDecision;
 
-        DMNResult dmnResult = dmnRuntime.evaluateAll(dmnModel, dmnContext);
-        
-        String partyDecision = (String) dmnResult.getDecisionResultByName("Party").getResult();
-        
-        return new PartyResponse(request.dayOfWeek, partyDecision);
+    public PartyResponse() {}
+
+    public PartyResponse(String dayOfWeek, String partyDecision) {
+      this.dayOfWeek = dayOfWeek;
+      this.partyDecision = partyDecision;
     }
-
-    public static class PartyRequest {
-        public String dayOfWeek;
-        
-        public PartyRequest() {}
-        
-        public PartyRequest(String dayOfWeek) {
-            this.dayOfWeek = dayOfWeek;
-        }
-    }
-
-    public static class PartyResponse {
-        public String dayOfWeek;
-        public String partyDecision;
-        
-        public PartyResponse() {}
-        
-        public PartyResponse(String dayOfWeek, String partyDecision) {
-            this.dayOfWeek = dayOfWeek;
-            this.partyDecision = partyDecision;
-        }
-    }
+  }
 }
